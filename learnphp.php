c1904a867cf78115392578ff1c0166a7aa6631cd
Attila Murvai <at.murvai@gmail.com>
2020-09-06 15:00:46 +0200

    add data submitter

--- learnphp.php
+++ learnphp.php
@@ -8,10 +8,22 @@
     <?php
 
     echo "<p>Data processed on: </>\n";
+    echo "<br>";
 
     date_default_timezone_set('UTC');
 
     echo date('h:i:s:u a, l F jS Y e');
+
+    $usersName = $_POST['username'];
+    $streetAddress = $_POST['streetaddress'];
+    $cityAddress = $_POST['cityaddress'];
+
+    echo "<br>";
+    echo "<br>";
+
+    echo $usersName . "</br>";
+    echo $streetAddress . "</br>";
+    echo $cityAddress . "</br>";
 
     ?>
 </body>
