f8e406ed1aa2187b688df354a20c2ff61047baf1
Attila Murvai <at.murvai@gmail.com>
2020-09-06 14:53:30 +0200

    init repo

---
+++ learnphp.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+    <title>Information Gathered</title>
+</head>
+
+<body>
+    <?php
+
+    echo "<p>Data processed on: </>\n";
+
+    date_default_timezone_set('UTC');
+
+    echo date('h:i:s:u a, l F jS Y e');
+
+    ?>
+</body>
+
+</html>
